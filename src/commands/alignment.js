b1ba528bf387d6673f6bbe581465afda0c6fc52d
Donna Murphy <donna.peplinskie@risevision.com>
2014-08-06 14:59:24 -0400

    Update regex and class names in alignment command

--- src/commands/alignment.js
+++ src/commands/alignment.js
@@ -1,27 +1,26 @@
 (function(wysihtml5) {
-  // TODO: Could also be wysiwyg-justify.
-  var REG_EXP = /wysiwyg-align-[a-z]+/g;
+  var REG_EXP = /(align-(left|center|right)|justify)/g;
 
   wysihtml5.commands.alignment = {
     exec: function(composer, command, alignment) {
       if (alignment === "justify") {
         return wysihtml5.commands.formatInline.exec(composer, command, "div",
-          "wysiwyg-justify", REG_EXP);
+          "justify", REG_EXP);
       }
       else  {
         return wysihtml5.commands.formatInline.exec(composer, command, "div",
-          "wysiwyg-align-" + alignment, REG_EXP);
+          "align-" + alignment, REG_EXP);
       }
     },
 
     state: function(composer, command, alignment) {
       if (alignment === "justify") {
         return wysihtml5.commands.formatInline.state(composer, command, "div",
-          "wysiwyg-justify", REG_EXP);
+          "justify", REG_EXP);
       }
       else  {
         return wysihtml5.commands.formatInline.state(composer, command, "div",
-          "wysiwyg-align-" + alignment, REG_EXP);
+          "align-" + alignment, REG_EXP);
       }
     }
   };

fc6fac7369f2cdb5b9fc4baeb7591a5f4d5f7f9f
Donna Murphy <donna.peplinskie@risevision.com>
2014-08-27 13:39:38 -0400

    Pass attrs parameter to lineHeight command

--- src/commands/lineHeight.js
+++ src/commands/lineHeight.js
@@ -1,15 +1,15 @@
 (function(wysihtml5) {
   var REG_EXP = /wysiwyg-line-height-[0-9\-]+/g;
-  
+
   wysihtml5.commands.lineHeight = {
-    exec: function(composer, command, lineHeight) {
+    exec: function(composer, command, lineHeight, attrs) {
       return wysihtml5.commands.formatInline.exec(composer, command, "span",
-        "wysiwyg-line-height-" + lineHeight, REG_EXP);
+        "wysiwyg-line-height-" + lineHeight, REG_EXP, attrs);
     },
 
-    state: function(composer, command, lineHeight) {
+    state: function(composer, command, lineHeight, attrs) {
       return wysihtml5.commands.formatInline.state(composer, command, "span",
-        "wysiwyg-line-height-" + lineHeight, REG_EXP);
+        "wysiwyg-line-height-" + lineHeight, REG_EXP, attrs);
     }
   };
 })(wysihtml5);
